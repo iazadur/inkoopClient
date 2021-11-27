8405d93a3b0aa2138222bfbdf3243a1a365e006e
iazadur <iamazadur@gmail.com>
2021-11-27 20:29:48 +0600

    finished

--- src/components/pages/Home/Navbar/Navbar.jsx
+++ src/components/pages/Home/Navbar/Navbar.jsx
@@ -3,12 +3,12 @@ import { MdOutlineViewHeadline } from 'react-icons/md'
 import './Navbar.css'
 
 const Navbar = () => {
-    const [mobile,setMobile]=useState(false)
+    const [mobile, setMobile] = useState(false)
     return (
         <div>
 
             <nav className="navbar">
-                <MdOutlineViewHeadline className="head" onClick={()=>setMobile(!mobile)} /> <div className="logo"><h1>LOGO</h1></div>
+                <MdOutlineViewHeadline className="head" onClick={() => setMobile(!mobile)} /> <div className="logo"><h1>LOGO</h1></div>
                 <div className="menu">
                     <a href="/" className="menuItem">Home</a>
                     <a href="/" className="menuItem">Products</a>
@@ -17,12 +17,17 @@ const Navbar = () => {
                 </div>
                 <div className="searchOption">
                     <div className="search-box">
-                        
+
                         <input type="text" className="input-search" placeholder="Type to Search..." />
                     </div>
                 </div>
             </nav>
-
+            {mobile && <div className="mobilemenu">
+                <a href="/" className="menuItem">Home</a>
+                <a href="/" className="menuItem">Products</a>
+                <a href="/" className="menuItem">About</a>
+                <a href="/" className="menuItem">Contact</a>
+            </div>}
         </div>
     );
 };
